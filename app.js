9c35ed866aa79f19609741154f7db1dbf2e1a00b
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2024-02-14 22:38:44 -0500

    avances parciales

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  let formDisplay = document.querySelector('.form');
+  let boton = document.querySelector('#add-button');
 
-const myLibrary = [];
 
-class Book {
-  constructor(title, author, pages, readed) {
+  const myLibrary = [];
+
+  class Book {
+    constructor(title, author, pages, readed) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readed = readed;
-  }
-}
-let book1 = new Book('Satanas', 'MarioMendoza', 200, 'yes');
-let book2 = new Book('Coronel', 'gabo', 300, 'yes');
-let book3 = new Book('pythonist', 'indian man', 380, 'yes')
-
-
-function addBookToLibrary(book, array) {
+    };
+  };
+  
+  function addBookToLibrary(book, array) {
   // do stuff here
-  array.push(book)
-};
-addBookToLibrary(book1, myLibrary);
-addBookToLibrary(book2, myLibrary);
-addBookToLibrary(book3, myLibrary);
-console.log(myLibrary[2]);
+    array.push(book)
+  };
 
-function displayBook(book){
-  const bookDetail = document.querySelector('.bookDetails');
-  bookDetail.innerHTML = `
-  <h1>${book.title} </h1>
-  <p>Author: ${book.author}</p>
-  <p>Pages: ${book.pages}</p>
-  <p>Read: ${book.readed}</p>
-  `
-};
 
-displayBook(book3);
+  function displayBook(book){
+ // do stuff here
+    
+  };
 
+  function showForm(){
+    boton.addEventListener('click', ()=>{
+      formDisplay.style.display = 'block';
+    })
+  };
+  showForm();
 });
 
 
