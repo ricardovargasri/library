f3a800b456ddd96e38d135d068e89e548d4d3529
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2024-02-20 11:15:52 -0500

    se arregla la funcionalidad, falta estilos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let formDisplay = document.querySelector('.form');
   let boton = document.querySelector('#add-button');
   let dialog = document.querySelector('#dialog');
+  let resetButton = document.querySelector('#reset-button');
 
   myLIbrary = [];
 
@@ -29,12 +30,59 @@ document.addEventListener("DOMContentLoaded", (event) => {
       e.target.readed.checked
     );
     myLIbrary.push(NewBook);
-    console.log(myLIbrary[myLIbrary.length - 1]);
+    displayBook(NewBook);
+    reset();
+    
+
+    /* console.log(myLIbrary.length);
+
+    console.log(myLIbrary[myLIbrary.length - 1]); */
 
 
     dialog.close();
     e.target.reset();
-  }) 
+  });
+  function displayBook(book){
+    const bookContainer = document.querySelector('#books-container');
+    
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    //book title
+    const bookTitle = document.createElement('h2');
+    bookTitle.innerText = book.title;
+    bookCard.appendChild(bookTitle);
+
+    //book's author
+    const bookAuthor = document.createElement('p');
+    bookAuthor.innerText = book.author;
+    bookCard.appendChild(bookAuthor);
+
+    //book's pages
+    const bookPages = document.createElement('p');
+    bookPages.innerText = book.pages;
+    bookCard.appendChild(bookPages);
+
+
+    const bookReaded = document.createElement('p');
+    bookReaded.innerText = book.readed? 'libro leido' : 'libro no leido';
+    bookCard.appendChild(bookReaded);
+
+    bookContainer.appendChild(bookCard);
+    
+
+  
+  }
+  function reset(){
+    resetButton.addEventListener('click', ()=>{
+      location.reload();
+    })
+
+    myLIbrary.length != 0 ? resetButton.style.display = 'block': resetButton.style.display = 'none';
+  };
+  
+  
+  
 
 });
 
