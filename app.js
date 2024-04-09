893262289fd9a6cf50cbe2e356c9392fa7f2e953
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2024-04-08 22:30:02 -0500

    se agregan nuevas funcionalidades con clases

--- app.js
+++ app.js
@@ -1,91 +1,47 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  let formDisplay = document.querySelector('.form');
-  let boton = document.querySelector('#add-button');
-  let dialog = document.querySelector('#dialog');
-  let resetButton = document.querySelector('#reset-button');
-
-  myLIbrary = [];
-
+document.addEventListener('DOMContentLoaded', ()=>{
+  const myLibrary = [];
 
   class Book {
-    constructor(title, author, pages, readed) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readed = readed;
+    constructor(title, author, large) {
+      this.title = title;
+      this.author = author;
+      this.large = large;
+
+    }
+    static renderBook(library){
+      let container = document.querySelector('#library-container');
+
+      container.innerHTML = '';
+      library.forEach(book =>{
+        let bookElement = document.createElement('div');
+        bookElement.className = 'books';
+
+        bookElement.innerHTML = `
+        <p>title: ${book.title}</p>
+        <p>author: ${book.author}</p>
+        <p>large: ${book.large}</p> 
+        `;
+        container.appendChild(bookElement);
+      });
     };
   };
 
-  boton.addEventListener('click', ()=>{
-    dialog.showModal();
-  });
-
-  formDisplay.addEventListener('submit', (e)=>{
+  var form = document.querySelector('#book-form');
+  form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    NewBook = new Book(
-      e.target.title.value,
-      e.target.author.value,
-      e.target.pages.value,
-      e.target.readed.checked
-    );
-    myLIbrary.push(NewBook);
-    displayBook(NewBook);
-    reset();
-    
+    const author = document.querySelector('#author');
+    const title = document.querySelector('#title');
+    const large = document.querySelector('#pagesNum');
 
-    /* console.log(myLIbrary.length);
+    let newBook = new Book(title.value, author.value, large.value)
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 
-    console.log(myLIbrary[myLIbrary.length - 1]); */
+    author.value = '';
+    title.value = '';
+    large.value = '';
 
-
-    dialog.close();
-    e.target.reset();
-  });
-  function displayBook(book){
-    const bookContainer = document.querySelector('#books-container');
-    
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book-card');
-
-    //book title
-    const bookTitle = document.createElement('h2');
-    bookTitle.innerText = book.title;
-    bookCard.appendChild(bookTitle);
-
-    //book's author
-    const bookAuthor = document.createElement('p');
-    bookAuthor.innerText = book.author;
-    bookCard.appendChild(bookAuthor);
-
-    //book's pages
-    const bookPages = document.createElement('p');
-    bookPages.innerText = book.pages;
-    bookCard.appendChild(bookPages);
-
-
-    const bookReaded = document.createElement('p');
-    bookReaded.innerText = book.readed? 'libro leido' : 'libro no leido';
-    bookCard.appendChild(bookReaded);
-
-    bookContainer.appendChild(bookCard);
-    
-
-  
-  }
-  function reset(){
-    resetButton.addEventListener('click', ()=>{
-      location.reload();
-    })
-
-    myLIbrary.length != 0 ? resetButton.style.display = 'block': resetButton.style.display = 'none';
-  };
-  
-  
-  
-
+    Book.renderBook(myLibrary);
+  }); 
 });
-
-
-
-
